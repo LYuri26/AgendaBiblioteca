0edf5a08a352f5c8c811d31ca6785ee27ee63a34
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-10-19 11:41:40 -0300

    19/10

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php 
+session_start(); // Inicie a sessão, se ainda não estiver iniciada
+
+/* Verifique se o parâmetro de erro é definido e se é igual a '1001'
+if (isset($_GET['error']) && $_GET['error'] == '1001') {
+  echo "<div class='failed' style='text-align: center; font-size:20px; font-weight:600;'>Login ou senha inválidos.</div>";
+}
+*/
+// Limpe todas as variáveis de sessão
+session_destroy();
+if (isset($_GET['error']) && $_GET['error'] == '1001') {
+  echo "<div class='failed' style='text-align: center; font-size:20px; font-weight:600;'>Login ou senha inválidos.</div>";
+}
+header("Location: index.php");
+exit();
+?>
